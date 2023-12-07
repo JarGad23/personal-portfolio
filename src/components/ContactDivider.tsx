c7cf5b905e221cd71dbd3edf3c36eeba9ce4591f
JarGad23 <j.gad@m8b.pl>
2023-12-07 17:32:27 +0100

    Created ContactDivider

---
+++ src/components/ContactDivider.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { ArrowDownToLine, ArrowRight, Github, Linkedin } from "lucide-react";
+
+export const ContactDivider = () => {
+  const sectionVariants = {
+    hidden: { y: 50, opacity: 0 },
+    visible: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <motion.section
+      initial="hidden"
+      animate="visible"
+      variants={sectionVariants}
+      transition={{ duration: 0.5 }}
+      className="w-full my-8 flex gap-x-4 gap-y-4 justify-center items-center flex-wrap"
+    >
+      <a
+        href="#contact"
+        className="bg-neutral-800 text-neutral-200 rounded-2xl px-8 py-3 font-semibold flex items-center hover:bg-neutral-600 transition"
+      >
+        Contact me
+        <ArrowRight className="h-4 w-4 ml-2" />
+      </a>
+      <a
+        download="/downloads/cv.pdf"
+        className="cursor-pointer bg-white shadow-md text-neutral-800 rounded-2xl px-8 py-3 font-semibold flex items-center hover:bg-neutral-100 transition"
+      >
+        Download CV
+        <ArrowDownToLine className="h-4 w-4 ml-2" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/jaros%C5%82aw-gad-270a93280/"
+        target="_blank"
+        className="cursor-pointer bg-white shadow-md text-neutral-800 p-3 rounded-full hover:bg-neutral-100 transition"
+      >
+        <Linkedin />
+      </a>
+      <a
+        href="https://github.com/JarGad23"
+        target="_blank"
+        className="cursor-pointer bg-neutral-800 shadow-md text-neutral-200 p-3 rounded-full hover:bg-neutral-600 transition"
+      >
+        <Github />
+      </a>
+    </motion.section>
+  );
+};
