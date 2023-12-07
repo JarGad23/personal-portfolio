f5a9a2d3913f883e06329690e64bcd4c2cf4a0f4
Jarosław Gad <127743272+JarGad23@users.noreply.github.com>
2023-12-07 17:33:26 +0100

    Feature 2: Imporove hero create divider (#2)
    * Added lucide react
    
    * Removed gradients
    
    * Created ContactDivider
    
    * Improvd Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { ContactDivider } from "@/components/ContactDivider";
 import { Hero } from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="w-full h-full px-4 md:px-8 max-w-7xl mx-auto flex flex-col items-center pt-28">
+    <div className="w-full h-full px-4 md:px-8 max-w-4xl mx-auto flex flex-col items-center pt-28">
       <Hero />
+      <ContactDivider />
     </div>
   );
 }
