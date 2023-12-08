9487126c80e1002a5bdd28dd2b7ac2cf8599fb94
JarGad23 <j.gad@m8b.pl>
2023-12-08 18:01:06 +0100

    Created tech stack and stack item components

---
+++ src/components/StackItem.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { StackType } from "../../data/stack";
+import { motion } from "framer-motion";
+
+type StackItemProps = {
+  item: StackType;
+  index: number;
+};
+
+export const StackItem = ({ item, index }: StackItemProps) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const sectionVariants = {
+    hidden: { y: 20, opacity: 0, boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)" },
+    visible: {
+      y: 0,
+      opacity: 0.6,
+      boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.3)",
+    },
+  };
+
+  const headingVariants = {
+    hidden: { y: 50, opacity: 0 },
+    visible: { y: 0, opacity: 1 },
+  };
+
+  const fadeInAnimationVariants = {
+    initial: {
+      opacity: 0,
+      y: 100,
+    },
+    animate: (index: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.05 * index,
+      },
+    }),
+  };
+
+  return (
+    <motion.div
+      variants={fadeInAnimationVariants}
+      initial="initial"
+      whileInView="animate"
+      viewport={{
+        once: true,
+      }}
+      custom={index}
+      onMouseEnter={() => setIsVisible(true)}
+      onMouseLeave={() => setIsVisible(false)}
+      className="relative flex items-center justify-center h-28 w-28 aspect-square bg-neutral-100 rounded-lg shadow-md"
+    >
+      <motion.div
+        initial="hidden"
+        animate={isVisible ? "visible" : "hidden"}
+        variants={sectionVariants}
+        transition={{ duration: 0.2 }}
+        className="absolute inset-0 bg-black z-10 flex items-center justify-center"
+      />
+      {isVisible && (
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={headingVariants}
+          transition={{ duration: 0.3 }}
+          className="absolute inset-0 z-50 flex items-center justify-center text-center"
+        >
+          <h3 className="font-bold text-xl text-neutral-100 ">{item.label}</h3>
+        </motion.div>
+      )}
+
+      <div className="relative h-20 w-20">
+        <Image
+          src={item.icon}
+          fill
+          alt="Tech Icon"
+          className="object-cover object-center"
+        />
+      </div>
+    </motion.div>
+  );
+};
---
+++ src/components/TechStack.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { stack } from "../../data/stack";
+import { StackItem } from "./StackItem";
+import { motion } from "framer-motion";
+
+export const TechStack = () => {
+  const sectionVariants = {
+    hidden: { y: 50, opacity: 0 },
+    visible: { y: 0, opacity: 1 },
+  };
+  return (
+    <motion.section
+      initial="hidden"
+      animate="visible"
+      variants={sectionVariants}
+      transition={{ duration: 0.5 }}
+      className="w-full flex flex-col items-center justify-center gap-y-8 mt-8"
+    >
+      <div className="bg-zinc-100 rounded-2xl px-8 py-3 opacity-90">
+        <h2 className="font-semibold">My Tech Stack</h2>
+      </div>
+      <div className="flex flex-wrap justify-center gap-8 max-w-3xl">
+        {stack.map((item, index) => (
+          <StackItem key={item.label} item={item} index={index} />
+        ))}
+      </div>
+    </motion.section>
+  );
+};
