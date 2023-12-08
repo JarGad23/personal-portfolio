1d0ffcb0bbf6d9a1f414164e108c1ae1174a4659
JarGad23 <j.gad@m8b.pl>
2023-12-08 18:01:14 +0100

    Added tech stack to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { ContactDivider } from "@/components/ContactDivider";
 import { Hero } from "@/components/Hero";
+import { TechStack } from "@/components/TechStack";
 
 export default function Home() {
   return (
     <div className="w-full h-full px-4 md:px-8 max-w-4xl mx-auto flex flex-col items-center pt-28">
       <Hero />
       <ContactDivider />
+      <TechStack />
     </div>
   );
 }
