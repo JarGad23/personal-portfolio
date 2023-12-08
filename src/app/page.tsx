97241fe3f833fc252f1928220723472e9f5dba6d
JarGad23 <j.gad@m8b.pl>
2023-12-08 18:19:30 +0100

    Created footer and added to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { TechStack } from "@/components/TechStack";
 
 export default function Home() {
   return (
-    <div className="w-full h-full px-4 md:px-8 max-w-4xl mx-auto flex flex-col items-center pt-28">
+    <div className="w-full px-4 md:px-8 max-w-4xl mx-auto flex flex-col items-center pt-28">
       <Hero />
       <ContactDivider />
       <TechStack />
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+  return (
+    <footer className="my-8 w-full  text-center text-gray-600 py-4">
+      <small>&copy; 2023 Jarosław. All rights reserved</small>
+      <p className="text-sm">
+        <span className="font-semibold">About this website: </span>built with
+        React & Next.js, Typescript, Tailwinnd CSS, Framer Motion, React Email,
+        hosted on Vercel.
+      </p>
+    </footer>
+  );
+};
