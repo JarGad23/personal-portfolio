84c86037b0a2aca5bc5227121b1bf3fad4b79da8
JarGad23 <j.gad@m8b.pl>
2023-12-08 18:00:53 +0100

    Created mocked data to display

---
+++ data/stack.ts
@@ -0,0 +1,59 @@
+export type StackType = {
+  label: string;
+  icon: string;
+};
+
+export const stack: StackType[] = [
+  {
+    label: "Html",
+    icon: "/svgs/html.svg",
+  },
+  {
+    label: "CSS",
+    icon: "/svgs/css.svg",
+  },
+  {
+    label: "Tailwind css",
+    icon: "/svgs/tailwind-css.svg",
+  },
+  {
+    label: "JavaScript",
+    icon: "/svgs/js.svg",
+  },
+  {
+    label: "TypeScript",
+    icon: "/svgs/typescript.svg",
+  },
+  {
+    label: "React",
+    icon: "/svgs/react.svg",
+  },
+  {
+    label: "Next.js",
+    icon: "/svgs/next-js.svg",
+  },
+  {
+    label: "Node.js",
+    icon: "/svgs/node.svg",
+  },
+  {
+    label: "Prisma",
+    icon: "/svgs/prisma.svg",
+  },
+  {
+    label: "MySql",
+    icon: "/svgs/mysql.svg",
+  },
+  {
+    label: "MongoDB",
+    icon: "/svgs/mongodb.svg",
+  },
+  {
+    label: "VS Code",
+    icon: "/svgs/vs-code.svg",
+  },
+  {
+    label: "Git",
+    icon: "/svgs/git.svg",
+  },
+];
