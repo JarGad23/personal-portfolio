31a294a7e2aaffa6016355c3df83e270a255fc6a
JarGad23 <j.gad@m8b.pl>
2023-12-09 16:35:44 +0100

    Added section ref to all sections

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,47 +1,78 @@
 "use client";
 
 import { FolderGit, Home, Layers, Mail, MessageCircle } from "lucide-react";
+import { motion } from "framer-motion";
+import { useActiveSectionContext } from "@/context/active-section-context";
+import { cn } from "../../libs/utils";
+import { SectionName } from "@/hooks/useSectionInView";
+
+const navRoutes = [
+  {
+    label: "Hero",
+    value: "hero",
+    href: "#hero",
+    Icon: <Home />,
+  },
+  {
+    label: "Tech Stack",
+    value: "tech-stack",
+    href: "#tech-stack",
+    Icon: <Layers />,
+  },
+  {
+    label: "Projects",
+    value: "projects",
+    href: "#projects",
+    Icon: <FolderGit />,
+  },
+  {
+    label: "Contact",
+    value: "contact",
+    href: "#contact",
+    Icon: <MessageCircle />,
+  },
+  {
+    label: "Email",
+    value: "email",
+    href: "#email",
+    Icon: <Mail />,
+  },
+];
 
 export const Navbar = () => {
-  const navRoutes = [
-    {
-      label: "Hero",
-      href: "#hero",
-      Icon: <Home />,
-    },
-    {
-      label: "Tech Stack",
-      href: "#tech-stack",
-      Icon: <Layers />,
-    },
-    {
-      label: "Projects",
-      href: "#projects",
-      Icon: <FolderGit />,
-    },
-    {
-      label: "Contact",
-      href: "#contact",
-      Icon: <MessageCircle />,
-    },
-    {
-      label: "Email",
-      href: "#email",
-      Icon: <Mail />,
-    },
-  ];
+  const { activeSection, setActiveSection, setTimeOfLasClick } =
+    useActiveSectionContext();
 
   return (
-    <nav className="w-full max-w-5xl sticky top-0 p-4 sm:my-4 sm:rounded-2xl bg-slate-100 shadow-md flex items-center sm:justify-around justify-center flex-wrap gap-x-6 gap-y-4 mx-auto z-50">
-      {navRoutes.map((route, index) => (
-        <a
-          className="text-sm sm:text-base font-semibold text-neutral-900 flex items-center gap-x-2"
-          href={route.href}
-        >
-          {route.Icon}
-          {route.label}
-        </a>
-      ))}
-    </nav>
+    <motion.nav
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      className="w-full max-w-5xl sticky top-0 p-4 sm:my-4 sm:rounded-2xl bg-slate-100 shadow-md  mx-auto z-50"
+    >
+      <ul className="flex items-center sm:justify-around justify-center flex-wrap gap-x-6 gap-y-4">
+        {navRoutes.map((route, index) => {
+          let isActive = route.value === activeSection;
+
+          return (
+            <li
+              key={route.value}
+              className={cn(
+                "text-sm sm:text-base font-semibold text-neutral-900",
+                isActive && "text-gray-400 underline"
+              )}
+              onClick={() => {
+                setActiveSection(route.value as SectionName);
+                setTimeOfLasClick(Date.now());
+              }}
+            >
+              <a href={route.href} className="flex items-center gap-x-2">
+                {route.Icon}
+                {route.label}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </motion.nav>
   );
 };
