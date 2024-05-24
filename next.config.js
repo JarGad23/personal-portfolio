bccc9db36777c309b43b1453170d41c5dad28f91
JarGad23 <j.gad@m8b.pl>
2024-05-24 15:58:16 +0200

    Pacakge json and config changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "personal-portfolio-nine-orpin.vercel.app"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "personal-portfolio-nine-orpin.vercel.app",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
   },
 };
 
