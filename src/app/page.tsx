b408501d752046e12499029813def2317f279706
Jarosław Gad <127743272+JarGad23@users.noreply.github.com>
2024-05-27 10:31:45 +0200

    Reduce mobile animations (#17)
    * Package json changes
    
    * Reduced Mobile animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,27 @@
+"use client";
+
+import { MotionConfig } from "framer-motion";
 import { Contact } from "@/components/Contact";
 import { ContactDivider } from "@/components/ContactDivider";
 import { Hero } from "@/components/Hero";
 import { Projects } from "@/components/Projects";
 import { TechStack } from "@/components/TechStack";
+import { useWindowSize } from "react-use";
 
 export default function Home() {
+  const { width } = useWindowSize();
+
+  console.log(width);
+
   return (
-    <div className="w-full px-4 md:px-8 mx-auto flex flex-col items-center mt-12">
-      <Hero />
-      <ContactDivider />
-      <TechStack />
-      <Projects />
-      <Contact />
-    </div>
+    <MotionConfig reducedMotion={width < 786 ? "always" : "never"}>
+      <div className="w-full px-4 md:px-8 mx-auto flex flex-col items-center mt-12">
+        <Hero />
+        <ContactDivider />
+        <TechStack />
+        <Projects />
+        <Contact />
+      </div>
+    </MotionConfig>
   );
 }
